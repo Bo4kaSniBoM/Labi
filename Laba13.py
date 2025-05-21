d0189129f0c5fea6ddbec870041fe69eb771c9e9
kap2z <118477653+kap2z@users.noreply.github.com>
2025-05-21 23:45:04 +0300

    Зміни від Лисаченка

--- Laba13.py
+++ Laba13.py
@@ -35,3 +35,31 @@ except json.JSONDecodeError as e:
     print(f"[Error] Помилка у форматі JSON: {e}")
 except Exception as e:
     print(f"[Error] Інша помилка: {e}")
+
+# Студент 2: Лисаченко П.А.
+
+try: 
+
+#Додаємо нового студента
+    with open(json_file, mode='r+', encoding='utf-8') as file:
+        students_json = json.load(file)
+        students_json.append({'Name': 'Dartanyan', 'Age': 21, 'Grade': 100})
+        file.seek(0)
+        json.dump(students_json, file, indent=4)
+        file.truncate()
+    print(f"[OK] Dartanyan додано до JSON файлу")
+except Exception as e:
+    print(f"[Error] Помилка додавання до JSON: {e}")
+
+#Копіюємо оновлених студентів з JSON до CSV
+try:
+    with open(json_file, mode='r', encoding='utf-8') as file:
+        updated_students = json.load(file)
+
+    with open(csv_file, mode='w', newline='', encoding='utf-8') as file:
+        writer = csv.DictWriter(file, fieldnames=['Name', 'Age', 'Grade'])
+        writer.writeheader()
+        writer.writerows(updated_students)
+    print(f"[OK] Оновлені дані з JSON переписано у CSV файл")
+except Exception as e:
+    print(f"[Error] Помилка оновлення CSV: {e}")
